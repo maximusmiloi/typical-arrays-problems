d944e70c761f60dc6cacacc8c706c1947ee7dfc8
Maxim Trafimov <maximusmiloi@gmail.com>
2020-10-03 19:04:04 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    a = array.sort((a, b) => a - b);
+    b = a[0];
+    return b;
 }
 
 exports.max = function max (array) {
-  return 0;
+    a = array.sort((a, b) => b - a);
+    b = a[0];
+    return b;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    var summ = 0;
+    for (var i = 0; i < array.length; i++) {
+        a = summ += array[i];
+    }
+    return a / array.length;
 }
